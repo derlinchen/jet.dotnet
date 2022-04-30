fc7e136fe6366cfef83906d0e679dae7bdcee259
derlinchen <derlin_nj@163.com>
2022-04-30 20:11:44 +0800

    代码生成工具验证

--- jet/Controllers/BaseDicController.cs
+++ jet/Controllers/BaseDicController.cs
@@ -5,54 +5,51 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace jet.Controllers
 {
+	[Route("jet/baseDic")]
+	[ApiController]
+	public class BaseDicController : ControllerBase
+	{
+		private readonly IBaseDicService _baseDicService;
+		public BaseDicController(IBaseDicService baseDicService)
+		{
+			_baseDicService = baseDicService;
+		}
 
-    [Route("jet/baseDic")]
-    [ApiController]
-    public class BaseDicController : ControllerBase
-    {
+		[HttpPost("saveBaseDic")]
+		public void SaveBaseDic([FromBody] BaseDicDto item)
+		{
+			_baseDicService.SaveBaseDic(item);
+		}
 
-        private readonly IBaseDicService _baseDicService;
+		[HttpDelete("deleteBaseDic")]
+		public void DeleteBaseDic(string id)
+		{
+			_baseDicService.DeleteBaseDic(id);
+		}
 
-        public BaseDicController(IBaseDicService baseDicService)
-        {
-            _baseDicService = baseDicService;
-        }
+		[HttpPost("updateBaseDic")]
+		public void UpdateBaseDic([FromBody] BaseDicDto item)
+		{
+			_baseDicService.UpdateBaseDic(item);
+		}
 
-        [HttpPost("saveBaseDic")]
-        public void SaveBaseDic([FromBody] BaseDicDto item)
-        {
-            _baseDicService.SaveBaseDic(item);
-        }
+		[HttpGet("getBaseDic")]
+		public BaseDicVo GetBaseDic(string id)
+		{
+			return _baseDicService.GetBaseDic(id);
+		}
 
-        [HttpDelete("deleteBaseDic")]
-        public void DeleteBaseDic(string id)
-        {
-            _baseDicService.DeleteBaseDic(id);
-        }
+		[HttpPost("getBaseDicList")]
+		public List<BaseDicVo> GetBaseDicList([FromBody] BaseDicDto item)
+		{
+			return _baseDicService.GetBaseDicList(item);
+		}
 
-        [HttpPost("updateBaseDic")]
-        public void UpdateBaseDic([FromBody] BaseDicDto item)
-        {
-            _baseDicService.UpdateBaseDic(item);
-        }
+		[HttpPost("searchBaseDic")]
+		public PageInfo<BaseDicVo> SearchBaseDic([FromBody] PageSearch<BaseDicDto> item)
+		{
+			return _baseDicService.SearchBaseDic(item);
+		}
 
-        [HttpGet("getBaseDic")]
-        public BaseDicVo GetBaseDic(string id)
-        {
-            return _baseDicService.GetBaseDic(id);
-        }
-
-        [HttpPost("getBaseDicList")]
-        public List<BaseDicVo> GetBaseDicList([FromBody] BaseDicDto item)
-        {
-            return _baseDicService.GetBaseDicList(item);
-        }
-
-        [HttpPost("searchBaseDic")]
-        public PageInfo<BaseDicVo> SearchBaseDic([FromBody] PageSearch<BaseDicDto> item)
-        {
-            return _baseDicService.SearchBaseDic(item);
-        }
- 
-    }
+	}
 }
